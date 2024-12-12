231a74d3c1c68345772e7c769ec29b0231f41d87
HNABLI <nablihatem3@gmail.com>
2024-12-12 18:12:18 +0100

    Feat: implementation of the Sha1 encoding algorithm according to the RFC3174

--- include/Sha1/Sha1.hpp
+++ include/Sha1/Sha1.hpp
@@ -1,9 +1,67 @@
 #ifndef SHA1_HPP
 #define SHA1_HPP
 
+/**
+ * @file Sha1.hpp
+ * 
+ * This module represent a declaration of Sha1 functions
+ * 
+ * © 2024 by Hatem Nabli
+ */
+#include <string>
+#include <stdint.h>
+#include <vector>
 
 namespace Sha1 {
     
+    /**
+     * This calculate the SHA-1 message digest of a given data.
+     * 
+     * @param[in] data
+     *      This is the input data for which to calculate the SHA-1
+     *      message digest.
+     * @return
+     *      returns the SHA1 message digest of the given data as
+     *      a string of hexadicimal digits. 
+     */
+    std::string Sha1String(const std::string& data);
+
+    /**
+     * This calculate the SHA-1 message digest of a given data.
+     * 
+     * @param[in] data
+     *      This is the input data for which to calculate the SHA-1
+     *      message digest.
+     * @return
+     *      returns the SHA1 message digest of the given data as
+     *      a string of hexadicimal digits. 
+     */
+    std::string Sha1String(const std::vector< uint8_t>& data);
+
+    /**
+     * This calculate the SHA-1 message digest of a given data.
+     * 
+     * @param[in] data
+     *      This is the input data for which to calculate the SHA-1
+     *      message digest.
+     * @return
+     *      returns the SHA1 message digest of the given data as
+     *      as a vector of hexadicimal digits. 
+     */
+    std::vector< uint8_t > Sha1Bytes(const std::vector< uint8_t >& data);
+
+        /**
+     * This function computes the SHA-1 message digest of the given data.
+     *
+     * @param[in] data
+     *     This is the data for which to compute the message digest.
+     *
+     * @return
+     *     The SHA1 message digest of the given data is returned
+     *     as a vector of bytes.
+     */
+    std::vector< uint8_t > Sha1Bytes(const std::string& data);
+
 }
 
 #endif /* SHA1_HPP */
